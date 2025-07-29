cbbfed8efc6b36d91bd758b2fed025345bcc81cf
raprettyri <riskafazilla7@gmail.com>
2025-07-29 21:45:10 +0700

    cloudflare R2
    fix cloudflare R2 link

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'pub-84311989504841c683e65d530fd7a8d1.r2.dev',
+        hostname: 'pub-544161ac01a34681bc8008830d4d5756.r2.dev',
         port: '',
         pathname: '/**',
       },
